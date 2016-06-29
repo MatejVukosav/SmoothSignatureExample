39b5866ced4eb795f94ead4606fb849ba27405c9
Matej Vukosav <mvukosav@cs.hr>
2016-06-29 16:10:41 +0200

    finished

---
+++ app/src/main/java/vuki/com/smoothsignatureexercise/MainActivity.java
@@ -0,0 +1,27 @@
+package vuki.com.smoothsignatureexercise;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import vuki.com.smoothsignatureexercise.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        binding = DataBindingUtil.setContentView( this, R.layout.activity_main );
+
+        binding.cleanAllBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                binding.signature.clear();
+            }
+        } );
+    }
+
+}
